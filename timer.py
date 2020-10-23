fe8aeb393c0742948a43cc44a756acb4f3fe11b4
sw1jari <jari@sw1.xyz>
2020-10-23 08:04:53 +0200

    first version

---
+++ timer.py
@@ -0,0 +1,74 @@
+from time import sleep
+import os
+
+#TODO: Implement this in another language that can run natively on android lmaooo
+
+class Timer:
+    def __init__(self, sets, set_time=0, supersets=0, set_rest_time=0, super_rest_time=0):
+        self.sets = sets
+        self.set_time = set_time
+        self.supersets = supersets
+        self.set_rest_time = set_rest_time
+        self.super_rest_time = super_rest_time
+
+    def setup(self):
+        if not sets:
+            self.sets = int(input("How many sets do you want to do? "))
+        if not set_time:
+            self.set_time = int(input("How long do you want your sets to be? "))
+        if not set_rest_time:
+            self.set_rest_time = int(input("How much rest between sets do you want? "))
+        if not supersets:
+            self.supersets = int(input("How many supersets do you want to do? "))
+        if not super_rest_time:
+            self.super_rest_time = int(input("How much rest between supersets do you want? "))
+
+    def run(self):
+        input("Press ENTER to start.")
+        for x in range(self.supersets):
+            if x > 0:
+                self.countdown(self.super_rest_time, "Time for superset rest")
+            for i in range(3):
+                self.countdown(5, f"Starting set {i + 1} of {self.sets} in 5s")
+                self.countdown(self.set_time, "Go!")
+                self.countdown(self.set_rest_time, "Time to rest")
+        input("All done!\nPress ENTER to quit.")
+
+    def statusbar(self):
+        print(f"\rSets: {self.sets}        Set time (s): {self.set_time}        Supersets: {self.supersets}        "
+              f"Set rest time (s): {self.set_rest_time}        Superset rest time (s): {self.super_rest_time}")
+
+    def countdown(self, seconds, message):
+        if seconds is None:
+            return
+        for s in range(seconds):
+            clear()
+            self.statusbar()
+            print(message)
+            print(s + 1)
+            sleep(1)
+
+def regular(sets=0, set_time=0, supersets=0, set_rest_time=0, super_rest_time=0):
+    timer = Timer(sets, set_time, supersets, set_rest_time, super_rest_time)
+    timer.run()
+
+def seluyanov():
+    set_time = int(input("How long do you want your sets to be? (30-50s) "))
+    supersets = int(input("How many supersets do you want to do? (1-3)|(4-9) "))
+    timer = Timer(3, set_time, supersets, set_time, 600)
+    timer.setup()
+    timer.run()
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def menu():
+    choice = input("What kind of training do you want to do? (regular, seluyanov, abs) ")
+    if choice == 'seluyanov':
+        seluyanov()
+    elif choice == 'regular':
+        regular()
+    elif choice == 'abs':
+        regular(8, 60, 1, 3, None)
+
+menu()
